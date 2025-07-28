2a669d017e42e9fa1be2209021f85089e5427d4d
Udonburo <192872686+Udonburo@users.noreply.github.com>
2025-07-28 10:30:34 +0900

    docs(iac): add commands and outputs (#30)

---
+++ infra/model_storage.tf
@@ -0,0 +1,18 @@
+resource "google_storage_bucket" "model_storage" {
+  name                        = "picca-models"
+  project                     = var.project
+  location                    = var.region
+  storage_class               = "NEARLINE"
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 90
+    }
+
+    action {
+      type          = "SetStorageClass"
+      storage_class = "COLDLINE"
+    }
+  }
+}
