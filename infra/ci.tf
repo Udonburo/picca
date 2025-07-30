bdbce605a5e511aef73eedb1b0a65f49204d3371
Udonburo <192872686+Udonburo@users.noreply.github.com>
2025-07-30 12:19:14 +0900

    chore(iac): fix workflow and triggers

--- infra/ci.tf
+++ infra/ci.tf
@@ -35,3 +35,41 @@ resource "google_cloudbuild_trigger" "main-branch-trigger" {
 
   filename = "infra/cloudbuild.yaml"
 }
+
+# Trigger for infrastructure changes via iac/ branches
+resource "google_cloudbuild_trigger" "iac-branch-trigger" {
+  provider = google-beta
+  project  = local.project_id
+  location = local.region
+  name     = "iac-branch-trigger"
+
+  service_account = "projects/${local.project_id}/serviceAccounts/terraform-sa@${local.project_id}.iam.gserviceaccount.com"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.picca_repository.id
+    push {
+      branch = "^iac/.*"
+    }
+  }
+
+  filename = "infra/cloudbuild-iac.yaml"
+}
+
+# Trigger for infrastructure tags like infra-*
+resource "google_cloudbuild_trigger" "iac-tag-trigger" {
+  provider = google-beta
+  project  = local.project_id
+  location = local.region
+  name     = "iac-tag-trigger"
+
+  service_account = "projects/${local.project_id}/serviceAccounts/terraform-sa@${local.project_id}.iam.gserviceaccount.com"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.picca_repository.id
+    push {
+      tag = "^infra-.*"
+    }
+  }
+
+  filename = "infra/cloudbuild-iac.yaml"
+}
